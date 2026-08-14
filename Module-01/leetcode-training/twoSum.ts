d7ee8bfea707ce77cdb822cd76124c2820983830
Imam Nurdin <nur.imamali@hotmail.com>
2026-08-14 15:56:13 +0700

    penambahan label di twosum.ts

--- Module-01/leetcode-training/twoSum.ts
+++ Module-01/leetcode-training/twoSum.ts
@@ -2,6 +2,8 @@
 // e.g: input: nums = [2,7,11,15], target = 9,
 // maka outputnya adalah: [0, 1]
 
+// LEVEL : Easy 
+
 function twoSum(nums: number[], target: number): number[] {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
